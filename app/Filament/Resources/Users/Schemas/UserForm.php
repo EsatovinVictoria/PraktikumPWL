60af9686ddf7404dd23bb64b31c29e9a9d8a3606
Esatovin Ebenhaezer Victoria <esatovinv@gmail.com>
2026-03-19 12:21:34 +0700

    first commit

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+                TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('email')
+                ->required()
+                ->email()
+                ->unique()
+                ->maxLength(255),
+
+                TextInput::make('password')
+                ->password()
+                ->required()
+                ->minLength(6),
+            ]);
+    }
+}
